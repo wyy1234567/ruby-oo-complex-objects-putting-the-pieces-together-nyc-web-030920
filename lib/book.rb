8bcb37633f74b42402cb16eba5e353153760fa7f
Yueying Wang <wangyy395@gmail.com>
2020-03-10 17:52:48 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,1 +1,12 @@
+class Book
+    attr_accessor :book_title, :author, :page_count, :genre
+
+    def initialize(title)
+        book_title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
